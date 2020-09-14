e424f091723cc4ab8e6a233841303c16bc7c3fba
leigharobinson <lmarobinson24@gmail.com>
2020-09-14 16:45:28 -0500

    created a loop to just get back songs where age range is correct

--- musicmemorydata.session.sql
+++ musicmemorydata.session.sql
@@ -7,10 +7,6 @@ VALUES
     ('5', 'Strongly Agree');
 
 
-<<<<<<< HEAD
-DELETE FROM musicmemoryapi_patientsong
-WHERE ID = 1;
-=======
 
 DELETE FROM musicmemoryapi_patientsong
 WHERE ID = 1;
@@ -19,4 +15,3 @@ DELETE From musicmemoryapi_patient
 WHERE ID = 4;
 
 DELETE FROM musicmemoryapi_songresponse
->>>>>>> 66d9f9adb598ed416eba13ae8f906bab971b1e72
